3973d764efc3448ee07c099cb0878528852b1882
sarfaraz <sarfaraz@vm-win-vs>
2018-04-05 12:30:32 +0530

    Added code to simulate SBM-Queus

---
+++ Azure/Azure Codes/SBM/Receiver/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string QueueName = "sbmQueue1";
+
+            string connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            var client =
+                QueueClient.CreateFromConnectionString(connectionString, QueueName);
+
+            // Event triggered by sender and recieved here
+            client.OnMessage(message =>
+            {
+                
+                Console.WriteLine(String.Format("Message body : {0}", message.GetBody<String>()));
+                Console.WriteLine(String.Format("Message id : {0}", message.MessageId));
+            });
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Azure/Azure Codes/SBM/Sender/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Azure;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string QueueName = "sbmQueue1";
+            string connectionString =
+                CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+
+            string msg = "This is a message from sender.";
+
+            var namespaceManager =
+                NamespaceManager.CreateFromConnectionString(connectionString);
+
+            if (!namespaceManager.QueueExists(QueueName))
+            {
+                namespaceManager.CreateQueue(QueueName);
+            }
+
+            MessagingFactory factory =
+                MessagingFactory.CreateFromConnectionString(connectionString);
+
+            QueueClient myQueueClient = factory.CreateQueueClient(QueueName);
+            BrokeredMessage bsm = new BrokeredMessage(msg);
+            myQueueClient.Send(bsm);
+
+            Console.ReadLine();
+        }
+    }
+}
