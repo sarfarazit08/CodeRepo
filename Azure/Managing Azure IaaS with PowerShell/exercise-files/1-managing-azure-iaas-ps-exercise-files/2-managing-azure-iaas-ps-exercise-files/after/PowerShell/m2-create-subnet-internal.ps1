9bbd5006aae95e14b41379e32a669ac02233eb0b
MD SARFARAZ <sarfarazit08@gmail.com>
2018-04-08 19:21:50 +0530

    updated exercise files
    updated exercise files

---
+++ Azure/Managing Azure IaaS with PowerShell/exercise-files/1-managing-azure-iaas-ps-exercise-files/2-managing-azure-iaas-ps-exercise-files/after/PowerShell/m2-create-subnet-internal.ps1
@@ -0,0 +1,44 @@
+﻿Param
+(
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $ResourceGroupName,    
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $Location,  
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $VnetName,    
+    [Parameter(Mandatory=$true)]
+    [Int] 
+    $SubnetIndex,   
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $LoadBalancerName,    
+    [Parameter(Mandatory=$true)]
+    [String] 
+    $AddressPrefix
+)
+
+$vnet = Get-AzureRmVirtualNetwork -Name $VnetName -ResourceGroupName $ResourceGroupName
+Add-AzureRmVirtualNetworkSubnetConfig -VirtualNetwork $vnet -Name "$LoadBalancerName-backend" `
+                                      -AddressPrefix $AddressPrefix 
+$vnet = Set-AzureRmVirtualNetwork -VirtualNetwork $vnet 
+
+$frontEndIp = New-AzureRmLoadBalancerFrontendIpConfig -Name "$LoadBalancerName-frontend" `
+                                                      -PrivateIpAddress 10.0.1.50 `
+                                                      -SubnetId $vnet.Subnets[$SubnetIndex].Id 
+
+$backEndPool = New-AzureRmLoadBalancerBackendAddressPoolConfig -Name "$LoadBalancerName-backend"
+
+$healthProbe = New-AzureRmLoadBalancerProbeConfig -Name "$LoadBalancerName-probe" `
+                                                  -Protocol Tcp -Port 8080 `
+                                                  -IntervalInSeconds 10 -ProbeCount 3
+
+$lbRule = New-AzureRmLoadBalancerRuleConfig -Name "HTTP" -FrontendIpConfiguration $frontEndIp `
+                                            -BackendAddressPool $backEndPool -Probe $healthProbe `
+                                            -Protocol Tcp -FrontendPort 8080 -BackendPort 8080
+
+New-AzureRmLoadBalancer -ResourceGroupName $ResourceGroupName -Location $Location -Name $LoadBalancerName `
+                        -FrontendIpConfiguration $frontEndIp -BackendAddressPool $backEndPool `
+                        -Probe $healthProbe -LoadBalancingRule $lbRule
---
+++ Azure/Managing Azure IaaS with PowerShell/exercise-files/1-managing-azure-iaas-ps-exercise-files/2-managing-azure-iaas-ps-exercise-files/before/dotnet/Sixeyed.Iaas/src/Sixeyed.Iaas.WebApp/Models/ServerDiagnostics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sixeyed.Iaas.WebApp.Models
+{
+    public class ServerDiagnostics
+    {
+        public string ApplicationName { get; set; }
+
+        public string ApplicationVersionNumber { get; set; }
+
+        public string WorkingDirectory { get; set; }
+
+        public string DnsHostName { get; set; }
+
+        public string MachineName { get; set; }
+
+        public DateTime MachineDate { get; set; }
+
+        public string MachineCulture { get; set; }
+
+        public string MachineTimeZone { get; set; }
+
+        public IEnumerable<string> IpAddressList { get; set; }
+    }
+}
